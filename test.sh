05eb0380caafe8fddb607fd936d076431747a75b
Chris <chrisrentsman601@gmail.com>
2016-05-05 12:42:15 -0400

    Updated test script with comments.

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
-# unzip all of the test cases from ICPC and rename to be testCases
+
+# Take command line argument as name of problem, and compile
 arg=$1
 lc="${1,,}"
 javac ./${arg}/${arg}.java -d .
+
+# Test each test case
 for file in ./testCases/${lc}/*.in; do
     fname="$(basename ${file%.*})"
     echo ${fname}
     java Catering < ${file} > ./testCases/${lc}/${fname}.out
     diff ./testCases/${lc}/${fname}.out ./testCases/${lc}/${fname}.ans
 done
-rm ./testCases/${lc}/*.out
-rm ./*.class
+
+# Clean up
+rm ./testCases/${lc}/*.out ./*.class
 echo "Done."
